861ec2e8cee98df1cb6527557219474a12e9ee12
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-08-20 21:33:51 +0200

    proof of concept stream audio over websocket

--- client/index.ts
+++ client/index.ts
@@ -1,7 +1,11 @@
 // PROOF OF CONCEPT AUDIO BUFFER RECIEVER
+let ctx = new window.AudioContext();
+
+
 
 new WebSocket("http://127.0.0.1:8080").onmessage = (event) => {
-  console.log(typeof(JSON.parse(event.data).data[0]));
+
+  console.log(JSON.parse(event.data).data);
 }
 
 
--- runner/src/main.rs
+++
@@ -1,8 +0,0 @@
-
-use util::audio_tap;
-
-fn main() {
-  let _ = audio_tap();
-
-  println!("Hello, world!");
-}
